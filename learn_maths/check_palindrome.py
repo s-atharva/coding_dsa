905779a234da5fa0e6c0c6d4d699246a2271ff99
Atharva <atharvasarbhukan@gmail.com>
2023-06-10 16:32:34 +0530

    added check_palindrome files

---
+++ learn_maths/check_palindrome.py
@@ -0,0 +1,24 @@
+def reverse_number(number: int):
+    num = number
+    reverse_num = 0
+    while num != 0:
+        num_mod = num % 10
+        reverse_num = (reverse_num * 10) + num_mod
+        num = num // 10
+    return reverse_num
+
+
+def check_palindrome(number: int):
+    reverse = reverse_number(number)
+    if number == reverse:
+        return "Palindrome"
+    else:
+        return "Not a Palindrome"
+
+
+if __name__ == "__main__":
+    print(check_palindrome(number=121))
+
+# git add learn_maths/reverse_number.py learn_maths/reverse_number.png
+# git commit - m "added reverse_number files"
+# git push origin learn_mathsD
